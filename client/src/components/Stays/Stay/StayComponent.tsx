302aa16c3343c1d899ae1187b32acfa4c93bf37c
jfiora <sillonpeludo@gmail.com>
2023-03-28 00:53:44 -0300

    finish stay component

--- client/src/components/Stays/Stay/StayComponent.tsx
+++ client/src/components/Stays/Stay/StayComponent.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Stay } from '../../../utils/types/Stay.type';
+import starIcon from '../../../assets/star.svg';
 
 const StayComponent = (stay: Stay) => {
     return (
-        <div key={stay._id}>
-            <img src={stay.photo} />
-            {stay.superHost ? <span>superhost</span> : null}
-            <h3>{stay.beds}</h3>
-            <h3>{stay.rating}</h3>
-            <h1>{stay.title}</h1>
+        <div key={stay._id} className='stay-component'>
+            <section className='stay-component-image-section'>
+                <img src={stay.photo} />
+            </section>
+            <section className='stay-component-info-section'>
+                <div className='stay-component-title-info'>
+                    <p>
+                        {stay.superHost ? <span>super host</span> : null}
+                        <>
+                            {stay.type}
+                            {stay.beds ? `. ${stay.beds} beds` : null}
+                        </>
+                    </p>
+                    <div className='stay-component-title-rating'>
+                        <img src={starIcon} style={{ fill: 'red' }} />
+                        <p>{stay.rating}</p>
+                    </div>
+                </div>
+                <div className='stay-component-title'>
+                    <p>{stay.title}</p>
+                </div>
+            </section>
         </div>
     );
 };
